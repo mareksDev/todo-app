a9cf022f1e1c86b2271fa1e3a1a25cb6499181e3
Marek Salurand <MarekS@Mareks-MBP.lan>
2016-02-26 00:20:46 +0200

    add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,22 @@
+// var person = {
+// 	name : 'Mareks',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+var grades = [15, 88];
+
+function addGrade (gradesArr, newGrade) {
+	gradesArr.push(55);
+	debugger;
+}
+
+addGrade(grades);
+console.log(grades);
