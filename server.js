f7e52cc0ef2115310fc8889640392a90c1073fba
Marek Salurand <MarekS@Mareks-MBP.lan>
2016-02-25 23:28:51 +0200

    Add DELETE todos route

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
-// Get /todos/:id
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
@@ -46,6 +46,19 @@ app.post('/todos', function (req, res) {
 	todos.push(body);
 
 	res.json(body);
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
 
 app.listen(PORT, function(){
