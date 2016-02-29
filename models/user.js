fe5a4f4d519f0c05179e237028c888459d0332e4
Marek Salurand <MarekS@Mareks-MacBook-Pro.local>
2016-02-29 20:46:43 +0200

    add auth middleware and findbytoken clas method

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ var cryptojs = require('crypto-js');
 var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
-	var user =  sequelize.define('user', {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -59,10 +59,31 @@ module.exports = function(sequelize, DataTypes) {
 						}
 
 						resolve(user);
-						
+
 					}, function(e) {
 						reject();
 					});
+				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, rejcet) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty890');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+						user.findById(tokenData.id).then(function (user) {
+							if (user) {
+								resolve(user);
+							} else {
+								reject();
+							}
+						}, function (e) {
+							reject();
+						});
+					} catch (e) {
+						reject();
+					}
 				});
 			}
 		},
@@ -71,13 +92,16 @@ module.exports = function(sequelize, DataTypes) {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			},
-			generateToken: function (type) {
+			generateToken: function(type) {
 				if (!_.isString(type)) {
 					return undefined;
 				}
 
 				try {
-					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
 					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123').toString();
 					var token = jwt.sign({
 						token: encryptedData
@@ -91,6 +115,6 @@ module.exports = function(sequelize, DataTypes) {
 			}
 		}
 	});
-	
+
 	return user;
 };
