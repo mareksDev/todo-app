c14fca815e2653c313f89c82d50ce52b34277efb
Marek Salurand <MarekS@Mareks-MacBook-Pro.local>
2016-02-29 20:04:46 +0200

    add login route and authenticate class methrod

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
-module.exports = function (sequelize, DataTypes) {
-	return sequelize.define('user', {
+module.exports = function(sequelize, DataTypes) {
+	var user =  sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -13,7 +13,7 @@ module.exports = function (sequelize, DataTypes) {
 		},
 		salt: {
 			type: DataTypes.STRING
-		}, 
+		},
 		password_hash: {
 			type: DataTypes.STRING
 		},
@@ -21,9 +21,9 @@ module.exports = function (sequelize, DataTypes) {
 			type: DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
-				len: [7,100]
+				len: [7, 100]
 			},
-			set: function (value) {
+			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
 
@@ -34,17 +34,43 @@ module.exports = function (sequelize, DataTypes) {
 		}
 	}, {
 		hooks: {
-			beforeValidate: function (user, options) {
+			beforeValidate: function(user, options) {
 				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
 			}
 		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+
+						resolve(user);
+						
+					}, function(e) {
+						reject();
+					});
+				});
+			}
+		},
 		instanceMethods: {
-			toPublicJSON : function () {
+			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			}
 		}
 	});
-}
+	
+	return user;
+};
